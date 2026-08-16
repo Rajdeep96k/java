4cf34ffda989be82cbe5d4315fed3bb746f66763
Rajdeep Gusain <rajdeepgusain96k@gmail.com>
2026-08-16 15:27:09 +0530

    Control Statements

---
+++ Core Java/Control Statements/WhileLoop.java
@@ -0,0 +1,26 @@
+public class WhileLoop {
+      public static void main(String[] args) {
+        int i = 1; // 1 to 10
+
+        // System.out.println(i);
+        // i++;
+        // System.out.println(i);
+        // i++;
+        // System.out.println(i);
+        // i++;
+
+        // Loops --> while --> infinite
+
+        /*
+            while(expression) {
+                // do something
+
+            }
+        */
+
+        while(i < 10) { // boolean --> true / false
+            System.out.println(i); 
+            i++;
+        }
+    }
+}
